98bc9da6d31a46a585573a2b408b019428741b23
Harsh91221 <harshch91221@gmail.com>
2024-03-07 00:57:48 +0530

    CODE

--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -1,17 +1,24 @@
 const VideoTitle = ({ title, overview }) => {
-    return (
-      <div className="w-screen aspect-video pt-[20%] px-24 absolute text-white bg-gradient-to-r from-black">
-        <h1 className="text-6xl font-bold">{title}</h1>
-        <p className="py-6 text-lg w-1/4">{overview}</p>
-        <div className="">
-          <button className=" bg-white text-black p-3 px-5 text-xl rounded-lg hover:bg-opacity-80">
-             Play
-          </button>
-          <button className="mx-2  bg-gray-500 text-white p-3 px-4 text-xl bg-opacity-50 rounded-lg">
-            More Info
-          </button>
-        </div>
+  return (
+    <div className="w-screen aspect-video pt-[20%] px-24 absolute text-white bg-gradient-to-r from-black">
+      <h1 className="text-6xl font-bold">{title}</h1>
+      <p className="py-6 text-lg w-1/4">{overview}</p>
+      <div className="">
+        <button className="bg-white text-black p-3 px-5 text-xl rounded-lg hover:bg-opacity-80">
+          <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" viewBox="0 0 16 16" className="h-6 w-6 inline-block mr-2">
+            <path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393z" />
+          </svg>
+          Play
+        </button>
+
+        <button className="mx-2  bg-gray-500 text-white p-3 px-4 text-xl bg-opacity-50 rounded-lg">
+          <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" viewBox="0 0 16 16" className="h-6 w-6 inline-block mr-2">
+            <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z" />
+          </svg>
+          More Info
+        </button>
       </div>
-    );
-  };
-  export default VideoTitle;  
+    </div>
+  );
+};
+export default VideoTitle;  
--- src/hooks/useNowPlayingMovies.js
+++ src/hooks/useNowPlayingMovies.js
@@ -4,17 +4,25 @@ import { addNowPlayingMovies } from "../utils/movieSlice";
 import { useEffect } from "react";
 
 const useNowPlayingMovies = () => {
+    // Fetch Data from TMDB API and update store
     const dispatch = useDispatch();
-    const nowPlayingMovies = useSelector(store => store.movies.nowPlayingMovies);
+  
+    const nowPlayingMovies = useSelector(
+      (store) => store.movies.nowPlayingMovies
+    );
+  
     const getNowPlayingMovies = async () => {
-        const data = await fetch('https://api.themoviedb.org/3/movie/now_playing?page=1', API_OPTIONS);
-        const json = await data.json();
-        dispatch(addNowPlayingMovies(json.results));
-    }
+      const data = await fetch(
+        "https://api.themoviedb.org/3/movie/now_playing?page=1",
+        API_OPTIONS
+      );
+      const json = await data.json();
+      dispatch(addNowPlayingMovies(json.results));
+    };
+  
     useEffect(() => {
-        if (!nowPlayingMovies) {
-            getNowPlayingMovies();
-        }
+      !nowPlayingMovies && getNowPlayingMovies();
     }, []);
-}
-export default useNowPlayingMovies;
+  };
+  
+  export default useNowPlayingMovies;
