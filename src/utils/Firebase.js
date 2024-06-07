28cdac1b33a95ec7cbda5f1d67f87908f593f712
Harsh91221 <harshch91221@gmail.com>
2024-06-07 22:35:48 +0530

    initial commit

--- src/utils/Firebase.js
+++ src/utils/Firebase.js
@@ -1,9 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth';
-import 'firebase/compat/firestore';
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -21,4 +21,10 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const analytics = getAnalytics(app);  // Initialize Analytics if needed
+
+// Initialize Firebase services
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { auth, db, analytics };
